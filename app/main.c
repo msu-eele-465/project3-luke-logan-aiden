311ceeaa2b9e78670e57da11ab41a6033a88fa72
Logan Graham <logangraham@Logans-MacBook-Air.local>
2025-02-19 14:08:46 -0700

    Completed ISR's for status LED. Tested the interrupt triggers. Logan 19 FEB 2025

--- app/main.c
+++ app/main.c
@@ -3,8 +3,11 @@
 #include "keypad.h"
 #include "status-led.h"
 
-// constatns
+// constants
 bool locked = true;
+
+// set up status LED
+
 
 int main(void)
 {
@@ -14,6 +17,9 @@ int main(void)
     // Disable the GPIO power-on default high-impedance mdoe to activate
     // previously configure port settings
     PM5CTL0 &= ~LOCKLPM5;
+
+    init_status_led_timer(&locked_rgb);     // setup led status timer
+    __enable_interrupt();                   // Enable Maskable IQR
 
     while(locked)
     {
@@ -26,4 +32,14 @@ int main(void)
     }
 }
 
+#pragma vector = TIMER3_B0_VECTOR
+__interrupt void ISR_CRR0(void)
+{
+    status_led_timer_ccr0();
+}
 
+#pragma vector = TIMER3_B1_VECTOR
+__interrupt void ISR_higher_CCR(void)
+{
+    TB3_ISR_call(TB3IV);
+}
--- src/status-led.h
+++ src/status-led.h
@@ -29,15 +29,15 @@ struct RGB
     int blue;
 };
 
-//typedef struct rgb RGB;
+//typedef struct rgb RGB
 
 // RGB value definitions
 
-const struct RGB locked_rgb {196, 62, 29};
+struct RGB locked_rgb = {196, 62, 29};
 
 // Function defitions
 
-void init_status_led_timer(struct RGB init_status_led)
+void init_status_led_timer(struct RGB* init_rgb)
 {
     // Set RGB pins as outputs
     P2DIR |= _BITS0TO3;                 // set P2.0-3 to outputs
@@ -55,15 +55,15 @@ void init_status_led_timer(struct RGB init_status_led)
     TB3CCTL0 |= CCIE;                   // enable compare interrupt
     TB3CCTL0 &= ~CCIFG;                 // clear interrupt flag
 
-    TB3CCR1 = 4*init_status_led.red;    // set CCR1 to red duty cycle
-    TB3CCTL1 |= CCIE;                   // enable compare interrupt
+    TB3CCR1 = 4*(init_rgb->red);        // set CCR1 to red duty cycle
+    TB3CCTL1 |= CCIE;                   // enable compaxxsre interrupt
     TB3CCTL1 &= ~CCIFG;                 // clear interrupt flag
 
-    TB3CCR2 = 4*init_status_led.green;  // set CCR2 to green duty cycle
+    TB3CCR2 = 4*(init_rgb->green);     // set CCR2 to green duty cycle
     TB3CCTL2 |= CCIE;                   // enable compare interrupt
     TB3CCTL2 &= ~CCIFG;                 // clear interrupt flag
 
-    TB3CCR3 = 4*init_status_led.blue;   // set CCR3 to blue duty cycle
+    TB3CCR3 = 4*(init_rgb->blue);       // set CCR3 to blue duty cycle
     TB3CCTL3 |= CCIE;                   // enable compare interrupt
     TB3CCTL3 &= ~CCIFG;                 // clear interrupt flag
     return;
@@ -77,7 +77,7 @@ void status_led_timer_ccr0(void)
     return;
 }
 
-void status_led_timer_ccr1(void)
+void _status_led_timer_ccr1(void)
 {
     // set red color to low
     P2OUT &= ~BIT0;
@@ -85,7 +85,7 @@ void status_led_timer_ccr1(void)
     return;
 }
 
-void status_led_timer_ccr2(void)
+void _status_led_timer_ccr2(void)
 {
     // set red color to low
     P2OUT &= ~BIT1;
@@ -93,12 +93,30 @@ void status_led_timer_ccr2(void)
     return;
 }
 
-void status_led_timer_ccr3(void)
+void _status_led_timer_ccr3(void)
 {
     // set red color to low
     P2OUT &= ~BIT2;
     TB3CCTL3 &= ~CCIFG;                 // clear interrupt flag
     return;
+}
+
+void TB3_ISR_call(int IV)
+{
+    switch (IV) 
+    {
+        case 2:
+            _status_led_timer_ccr1();
+            return;
+
+        case 4:
+            _status_led_timer_ccr2();
+            return;
+
+        case 6:
+            _status_led_timer_ccr3();
+            return;
+    }
 }
 
 
