2404d2a332f59c6c7593869ee76481902d5584a3
lukej267 <lukej10.sfc@gmail.com>
2025-02-22 12:45:59 -0700

    tested pattern 6 0 rotate right

--- app/main.c
+++ app/main.c
@@ -6,12 +6,13 @@ int main(void)
     
     WDTCTL = WDTPW | WDTHOLD;               // Stop watchdog timer
     PM5CTL0 &= ~LOCKLPM5;                   // Disable the GPIO power-on default high-impedance mdoe to activate
-    //pattern_static();
-    //pattern_toggle();
-    //pattern_up_counter();
-    //pattern_in_out();
-    //pattern_down_counter();
-    pattern_rotate_left();
+    //pattern_static();                     // Pattern 0
+    //pattern_toggle();                     // Pattern 1
+    //pattern_up_counter();                 // Pattern 2
+    //pattern_in_out();                     // Pattern 3
+    //pattern_down_counter();               // Pattern 4
+    //pattern_rotate_left();                // Pattern 5
+    pattern_0_rotate_right();               // Pattern 6
 
     
     // Main loop
