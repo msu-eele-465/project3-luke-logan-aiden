7e4acb13f8cf8e7102686c2b547440f9588de1bf
lukej267 <lukej10.sfc@gmail.com>
2025-02-22 13:24:42 -0700

    implemented pattern 7 fill left

--- app/led_pattern.c
+++ app/led_pattern.c
@@ -164,14 +164,31 @@ _init_LED_bar();
     TB0CCTL0 |= CCIE;     // Enable TB0 CCR0 Overflow IRQ
     __enable_interrupt();   // Enable Maskable IRQs
 }
-/*
-    // If able to:
 
-    // Pattern 6
+void pattern_fill_left() {
+    _init_LED_bar();
 
+    P1OUT &= ~BIT0; // Initialize starting LEDs
+    P1OUT &= ~BIT1;
+    P1OUT &= ~BIT2;
+    P1OUT &= ~BIT3;
+    P1OUT &= ~BIT4;
+    P1OUT &= ~BIT5;
+    P1OUT &= ~BIT6;
+    P1OUT |= BIT7;
 
-    // Pattern 7
-*/
+    // Setup Timer
+    TB0CTL |= TBCLR;    // clear timers & dividers
+    TB0CTL |= TBSSEL__ACLK; // Source = ACLK
+    TB0CTL |= MC__UP;   // Mode=UP
+    TB0CCR0 = 32769;    // CCR0=32769
+
+    // Setup Timer Compare IRQ
+    TB0CCTL0 &= ~CCIFG;   // Clear CCR0 Flag
+    TB0CCTL0 |= CCIE;     // Enable TB0 CCR0 Overflow IRQ
+    __enable_interrupt();   // Enable Maskable IRQs
+    
+}
 
 
 //------ Interrupt Service Routines -----------------------------
@@ -180,31 +197,29 @@ __interrupt void ISR_TB0_CCR0(void)
 {
     counter++;
     switch(counter) {
-        case 1: P1OUT &= ~BIT1;
-                P1OUT |= BIT0;
+        case 1: P1OUT |= BIT6;
                 break;
-        case 2: P1OUT &= ~BIT2;
-                P1OUT |= BIT1;
+        case 2: P1OUT |= BIT5;
                 break;
-        case 3: P1OUT &= ~BIT3;
-                P1OUT |= BIT2;
+        case 3: P1OUT |= BIT4;
                 break;
-        case 4: P1OUT &= ~BIT4;
-                P1OUT |= BIT3;
+        case 4: P1OUT |= BIT3;
                 break;
-        case 5: P1OUT &= ~BIT5;
-                P1OUT |= BIT4;
+        case 5: P1OUT |= BIT2;
                 break;
-        case 6: P1OUT &= ~BIT6;
-                P1OUT |= BIT5;
+        case 6: P1OUT |= BIT1;
                 break;
-        case 7: P1OUT &= ~BIT7;
-                P1OUT |= BIT6;
+        case 7: P1OUT |= BIT0;
                 break;
         case 8: P1OUT &= ~BIT0;
+                P1OUT &= ~BIT1;
+                P1OUT &= ~BIT2;
+                P1OUT &= ~BIT3;
+                P1OUT &= ~BIT4;
+                P1OUT &= ~BIT5;
+                P1OUT &= ~BIT6;
                 P1OUT |= BIT7;
                 counter = 0;
-                break;
     }
     
     TB0CCTL0 &= ~CCIFG; //Clear CCR0 Flag
